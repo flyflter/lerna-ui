6803107c7cf7b2a99007aefaaa84e79d4e736bb2
zhongy-g <zhongy-g@glodon.com>
2021-12-03 18:10:48 +0800

    34545

--- packages/gpmbc-framework/lib/gpmbc-framework.js
+++ packages/gpmbc-framework/lib/gpmbc-framework.js
@@ -4,6 +4,6 @@ module.exports = gpmbcFramework;
 
 function gpmbcFramework() {
   const abc = 'aaaa'
-  console.log('gpmbcFramework----这个是测试9999')
+  console.log('gpmbcFramework----这个是测试99990909')
   // TODO
 }
